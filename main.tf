3f4298db6a04aca165ca2d87399d2217fb7a6033
Amanda Jennings <ajennings@hashicorp.com>
2018-06-21 14:41:56 -0400

    removing client id

--- main.tf
+++ main.tf
@@ -3,11 +3,7 @@ terraform {
 }
 
 # Configure the Azure Provider
-provider "azurerm" { 
-  subscription_id = "c0a607b2-6372-4ef3-abdb-dbe52a7b56ba"
-  client_id       = "c0a607b2-6372-4ef3-abdb-dbe52a7b56ba"
-  tenant_id       = "0e3e2e88-8caf-41ca-b4da-e3b33b6c52ec"
-}
+provider "azurerm" {}
 
 # Create a resource group
   resource "azurerm_resource_group" "network" {
