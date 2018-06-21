e8945c686164d11dca7ab320a4b83a1f48b18a50
Amanda Jennings <ajennings@hashicorp.com>
2018-06-21 14:25:24 -0400

    add azure resource group

--- main.tf
+++ main.tf
@@ -2,39 +2,34 @@ terraform {
   required_version = ">= 0.11.1"
 }
 
-variable "location" {
-  description = "Azure location in which to create resources"
-  default = "East US"
+# Configure the Azure Provider
+provider "azurerm" { }
+
+# Create a resource group
+  resource "azurerm_resource_group" "network" {
+  name     = "ajennings_testing"
+  location = "East US"
 }
 
-variable "windows_dns_prefix" {
-  description = "DNS prefix to add to to public IP address for Windows VM"
-}
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "network" {
+  name                = "production-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = "${azurerm_resource_group.network.location}"
+  resource_group_name = "${azurerm_resource_group.network.name}"
 
-variable "admin_password" {
-  description = "admin password for Windows VM"
-  default = "pTFE1234!"
-}
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
 
-module "windowsserver" {
-  source              = "Azure/compute/azurerm"
-  version             = "1.1.5"
-  location            = "${var.location}"
-  vm_hostname         = "pwc-ptfe"
-  admin_password      = "${var.admin_password}"
-  vm_os_simple        = "WindowsServer"
-  public_ip_dns       = ["${var.windows_dns_prefix}"]
-  vnet_subnet_id      = "${module.network.vnet_subnets[0]}"
-}
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.0.2.0/24"
+  }
 
-module "network" {
-  source              = "Azure/network/azurerm"
-  version             = "1.1.1"
-  location            = "${var.location}"
-  resource_group_name = "terraform-compute"
-  allow_ssh_traffic   = true
-}
-
-output "windows_vm_public_name"{
-  value = "${module.windowsserver.public_ip_dns_name}"
+  subnet {
+    name           = "subnet3"
+    address_prefix = "10.0.3.0/24"
+  }
 }
